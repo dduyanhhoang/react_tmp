5dde280cc8ca38a0aa04e3d2a6a0ec58cf5913cd
dduyanhhoang <dduyanhhoang@gmail.com>
2025-01-06 14:29:45 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header() {
+  return <h1>Hello World</h1>;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
